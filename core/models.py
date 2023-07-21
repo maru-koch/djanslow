74237948df0cf0251095dd22eb311e640c8ba2f0
= <=>
2023-07-21 11:47:16 +0100

    made corrections on the customer model

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ SEX = (('female', 'Female'), ('male', 'Male'))
 
 class Customer(models.Model):
     name = models.CharField(max_length=255)
-    sex = models.CharField(choices=SEX)
+    sex = models.CharField(choices=SEX, max_length=255)
     address = models.CharField(max_length=255)
 
     def __str__(self) -> str:
