31a95666b4ddd8def216ef37006915f7d7b5d819
= <=>
2023-07-21 11:51:57 +0100

    changed the funtion view to an api view

--- core/views.py
+++ core/views.py
@@ -2,14 +2,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from core.models import Customer
 from core.serializer import CustomerSerializer
+from rest_framework.views import APIView
 # Create your views here.
 
 def index_view(request):
     return Response({'status':status.HTTP_200_OK})
 
-def add_customer(request):
-    data = request.data
-    serializer = CustomerSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'status':status.HTTP_201_CREATED})
+class AddCustomerView(APIView):
+    def post(self, request):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':status.HTTP_201_CREATED})
